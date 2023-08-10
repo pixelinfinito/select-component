53cde46db325f431dc4e9893611867b54870cc16
edsonpixel <edson.bandola@pixel.ao>
2023-08-10 10:37:41 +0100

    feat: update lib

--- README.md
+++ README.md
@@ -1,51 +1,59 @@
-# Business Model Canvas Plugin for Budibase
-<img src="https://github.com/edsonpixel/budibase-businessmodel/blob/main/images/capa.png" />
-The Business Model plugin for Budibase is a tool that allows users to create, manage, and visualize business models within the Budibase platform. This plugin is designed to be intuitive and user-friendly, enabling users to easily add, edit, and view various business elements such as customer segments, value propositions, distribution channels, revenue streams, and costs.
+# Budibase Select Component
 
-## Getting Started
-To get started with the Business Model plugin, follow these simple steps:
-1. Install the plugin by navigating to the Budibase dashboard, selecting the Plugins tab, and  searching for "Business Model."
+<img src="https://github.com/pixelinfinito/budibase-select-component/blob/main/select.png?raw=true" />
+## Description
+It's a component to implement fullcalendar in your budibase.
 
-2. Once the plugin is installed, navigate to the Business Model tab within the Budibase platform.
-3. Add business elements to your model by selecting the relevant sections and entering the required information.
-4. Use the visualizer to view your model and how the various elements interconnect to form a complete business model.
+## Installation
+To install the plugin for Budibase, follow these steps:
 
-## Important information
-
-1. To use this plugin it is necessary to have a data source.
-2. When creating your data source, the table that will be used must have a column named "key".
-3. To populate the data in the table it is necessary to follow the list of keys, they are:
-    1. <b>keyPartners</b> = "Key Partners",
-    2. <b>keyResources</b> = "Key Resources",
-    3. <b>keyActivites</b> = "Key Activites",
-    4. <b>keyPropositions</b> = "Value Propositions",
-    5. <b>keyRelationships</b> = "Customer Relationships",
-    6. <b>keySegments</b> = "Customer Segments",
-    7. <b>keyStructure</b> = "Cost Structure",
-    8. <b>keyStreams</b> = "Revenue Streams",
-    9. <b>keyChannels</b> = "Channels"
-
-4. Example: If I want to put a text in the field "<b>Key Partners</b>" I have to use the key "<b>keyPartners</b>"
-
-# How to map the data
-![image](https://user-images.githubusercontent.com/116651657/230389709-e1980907-fd83-4f02-b84d-8d305d002c92.png)
-
-<p>This plugin allows you to map some data that will appear, such as title and description. You can define any database column to be mapped to these fields.</p>
+1. Copy the repository link ```https://github.com/pixelinfinito/budibase-select-component```.
+2. Open Budibase and navigate to the "Plugins" section.
+3. Click add plugin.
+4. Select GitHub source.
+5. Put the link in the URL section
 
 
- 
+<p>The plugin will be installed automatically.</p>
+
+## Use
+
+After installing the plugin for Budibase, you can start using it in your application. 
+The plugin adds select input and functionality to the platform.
+
+1. First add a data source
+2. Add the calendar component inside the data source, you can search for Budibase Select Component
+
 ## Features
-1. Easy-to-use interface for adding and managing business elements Real-time collaboration among users
-2. Possibility to map title and description
-3. Possibility to change the language. Portuguese and English
+
+1. Filter
+2. Define the name of the column that will be selected
+3. Disable input
+4. Call events on change
 
 
-If you have any questions or issues with the Business Model plugin, please reach out to our support team at dev@pixel.ao.
 
-Contributing
-We welcome contributions from the community. If you would like to contribute to the development of the Business Model plugin, please fork this repository and submit a pull request with your changes.
+## Contributing
+
+If you want to contribute to the development of the plugin, follow these steps:
+
+1. Fork this repository.
+2. Create a new branch with your changes: ```git checkout -b my-branch```
+3. Make the desired changes and commit: ```git commit -m 'my changes```
+4. Push your changes to your branch: ```git push origin my-branch```
+5. Open a Pull Request to the original repository.4
+
+
+## Instructions
+To build this plugin run the following in your Budibase CLI:
+
+```budi plugins --build```
+You can also re-build everytime you make a change to your plugin with the command:
+
+``` budi plugins --watch ```
+
+## Known issues
+
+There are currently no known issues. If you encounter any problems using the plugin for Budibase, please open an issue in this repository and describe the problem in detail.
 
 ## Find out more about [Budibase](https://github.com/Budibase/budibase).
-
-License
-The Business Model plugin is licensed under the MIT License.
--- src/Component.svelte
+++ src/Component.svelte
@@ -1,239 +1,54 @@
 <script>
-  
-  import { getContext } from "svelte"
-  import Content from "./components/Content.svelte";
-  import {keyItems} from "./store"
-  import LinkIcon from "./icons/link-3-svgrepo-com.svg"
-  import Chceck from "./icons/check-circle-svgrepo-com.svg"
-  import Gift from "./icons/Gift.svg"
-  import Heart from "./icons/heart-fill-svgrepo-com.svg"
-  import Male from "./icons/male-face-icon.svg"
-  import RourcesIcon from "./icons/manufacturing-process-engineer-icon.svg"
-  import Truck from "./icons/logistic-truck-icon.svg"
-  import Tags from "./icons/tags.svg"
-  import MoneyBag from "./icons/money-bag-change-money-capital-svgrepo-com.svg"
+  import { getContext } from "svelte";
+  import Select from 'svelte-select';
+  import { onMount } from "svelte";
+ 
+  const { styleable } = getContext("sdk"); 
+  const component = getContext("component");
 
-  import lang from "./util/lang"
-
-  export let language
-  
+  export let multiple;
   export let dataProvider
-  export let mappingTitle
-  export let mappingDescription
-  export let cardColor
-  let usageLanguage = lang['pt']
-  
-  export let clickEvent
+  export let label
+  export let disable
+  export let selectEvent
+  export let placeholder 
+  export let required
+  export let placeholderAlwaysShow
 
-  let data = []
+  let items = []
   
-  const keys = {
-    keyPartners: [],
-    keyResources: [],
-    keyActivites: [],
-    keyPropositions: [],
-    keyRelationships: [],
-    keySegments: [],
-    keyStructure: [],
-    keyStreams:[],
-    keyChannels:[]
-  }
-
-  $: {
-    
-    usageLanguage = lang[language ?? 'pt']
-    if(dataProvider.rows){
-      data = dataProvider.rows
-      let counter = 0
-      data.map(dataItem=>{
-        if(counter == 0) keys[dataItem.key] = []
-        keys[dataItem.key].push({
-          data: dataItem,
-          description: mappingDescription ? dataItem[mappingDescription] : dataItem.description,
-          title: mappingTitle ? dataItem[mappingTitle] : dataItem.title
-        })
-        counter += 1
+  async function fetchData() {
+    try {
+      const response = dataProvider;
+      
+      items = response?.rows?.map((item, index)=>{
+        return {
+          value: item[label],
+          label: item[label],
+          item: item 
+        }
       })
-      keyItems.set(keys)
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
   }
+ 
+  onMount(()=>{
+   fetchData()
+  })
 
-  async function handleClick(data){
-    await clickEvent({
-      data: JSON.parse(JSON.stringify(data.detail))
+  async function handleSelectChange(event) {
+    
+    const selectedValue = event.detail
+    const dataSend = JSON.parse(JSON.stringify(selectedValue?.item))
+    
+    await selectEvent({
+      data: dataSend
     })
   }
 
-  const { styleable } = getContext("sdk")
-  const component = getContext("component")
 </script>
 
-{#if dataProvider.rows.length > 0}
 <div use:styleable={$component.styles}>
-  <div class="container">
-		<div class="row">
-      <div  class="col">
-        <div class="head">
-          <span >
-            {@html LinkIcon}
-          </span>
-          <span> {usageLanguage.keyPartners }</span>
-        </div>
-        <Content color={cardColor ?? "#fdf092"} key={'keyPartners'} data={keys.keyPartners}   on:handleClick={(event)=> handleClick(event)} />
-      </div>
-      <div  class="col">
-        <div class="child-col">
-          <div class="head">
-            <span >
-              {@html Chceck}
-            </span>
-            <span> {usageLanguage.keyActivites}</span>
-          </div>
-          <Content color={cardColor ?? "#fdf092"} key={'keyActivites'} data={keys.keyActivites}   on:handleClick={(event)=> handleClick(event)} />
-        </div>
-        <div class="child-col">
-          <div class="head">
-            <span >
-              {@html RourcesIcon}
-            </span>
-            <span>{usageLanguage.keyResources}</span>
-          </div>
-          <Content color={cardColor ?? "#fdf092"} key={'keyResources'} data={keys.keyResources}   on:handleClick={(event)=> handleClick(event)} />
-        </div>
-      </div>
-      <div class="col">
-        <div class="head">
-          <span>
-            {@html Gift}
-          </span>
-          <span>{usageLanguage.keyPropositions}</span>
-        </div>
-        <Content  color={cardColor ?? "#fdf092"} key={'keyPropositions'} data={keys.keyPropositions}   on:handleClick={(event)=> handleClick(event)} />
-      </div>
-      <div class="col">
-        <div class="child-col">
-          <div class="head">
-            <span >
-              {@html Heart}
-            </span>
-            <span>{usageLanguage.keyRelationships}</span>
-          </div>
-          <Content color={cardColor ?? "#fdf092"} key={'keyRelationships'} data={keys.keyRelationships}   on:handleClick={(event)=> handleClick(event)} />
-        </div>
-        <div class="child-col">
-          <div class="head">
-            <span >
-              {@html Truck}
-            </span>
-            <span>{usageLanguage.keyChannels} </span>
-          </div>
-          <Content color={cardColor ?? "#fdf092"} key={'keyChannels'} data={keys.keyChannels}   on:handleClick={(event)=> handleClick(event)} />
-        </div>
-      </div>
-      <div class="col">
-        <div class="head">
-          <span >
-            {@html Male}
-          </span>
-          <span>{usageLanguage.keySegments}</span>
-        </div>
-        <Content color={cardColor ?? "#fdf092"} key={'keySegments'} data={keys.keySegments}   on:handleClick={(event)=> handleClick(event)} />
-      </div>
-		</div>
-	
-		<div class="row">
-      <div class="col">
-        <div class="head">
-          <span >
-            {@html Tags}
-          </span>
-          <span>{usageLanguage.keyStructure}</span>
-        </div>
-        <Content color={cardColor ?? "#fdf092"} key={'keyStructure'} data={keys.keyStructure}   on:handleClick={(event)=> handleClick(event)} />
-      </div>
-			<div class="col">
-        <div class="head">
-          <span >
-            {@html MoneyBag}
-          </span>
-          <span>{usageLanguage.keyStreams}</span>
-        </div>
-        <Content color={cardColor ?? "#fdf092"} key={'keyStreams'} data={keys.keyStreams}   on:handleClick={(event)=> handleClick(event)} />
-			</div>
-		</div>
-  </div>
+  <Select disabled={disable} placeholderAlwaysShow={placeholderAlwaysShow} required={required} {items} placeholder={placeholder} multiple={multiple} class="foo bar" on:change={handleSelectChange} />
 </div>
-{/if}
-<style>
-	.container{
-		max-width: 100%;
-		width: 100%;
-		border: 1px solid #cdcfd2;
-		max-height: 600px;
-    border-radius: 1px;
-		height: 600px;
-		display: flex;
-    background-color: #FFF;
-		flex-direction: column;
-    box-sizing: border-box;  
-	}
-	.container .row:last-child{
-		border-top: 1px solid #cdcfd2;
-		height: 30%;
-		display: flex;
-		justify-content: space-between;
-	}
-	.row{
-		font-size: 0;
-	}
-	.container .row:last-child .col:first-child{
-		border-right: 1px solid #cdcfd2;
-	}
-	.container .row:first-child{
-		display: flex;
-		height: 70%;
-		justify-content: space-between;
-	}
-	.container .row:first-child  .col:nth-child(2), 
-	.container .row:first-child  .col:nth-child(4){
-		border-left: 1px solid #cdcfd2;
-		border-right: 1px solid #cdcfd2;
-	}
-	:global(.container .row:first-child  .col, 
-	.container .row:last-child .col){
-		flex: 0 1 auto;
-    overflow-wrap: break-word; 
-	}
-  
-	.container .row:first-child  .col{
-		max-width: calc(100%/5);
-	}
-  .container .row:last-child  .col{
-		max-width: calc(100%/2);
-	}
-  .container .row:last-child  .col,
-  .container .row:first-child  .col{
-    width: 100%;
-  }
-  .container .row:first-child  .col .child-col:first-child{
-    border-top: 0;
-  }
-	.col .child-col{
-		height: 50%;
-		border-top: 1px solid #cdcfd2;
-	}
-	.col span, .child-col span{
-		text-align: left;
-		font-weight: bold;
-		font-size: 13px;
-    color:#000;
-	}
-	.col, .child-col{
-		text-indent: 9px;
-	}
-  .head{
-    padding: 8px;
-    display: flex;
-    align-items: center;
-  }
-</style>
